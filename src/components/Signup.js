11aacfbd77719f3d68622c7465efa5f09c0965b2
piyushsakhare <piyushsakhare713@gmail.com>
2022-03-26 13:07:42 +0530

    temp changes

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -3,6 +3,8 @@ function Signup() {
         <div className='grid place-content-center my-24 ' >
             <div className="p-8 bg-gray-200">
                 <form className="flex-col flex w-96">
+                <label>First Name : </label>
+                    <input className="p-2 mb-6 border border-black" type='email' placeholder="Enter Email" ></input>
                     <label>Email : </label>
                     <input className="p-2 mb-6 border border-black" type='email' placeholder="Enter Email" ></input>
                     <label>Password : </label>
